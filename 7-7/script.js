c8740f8a4dd3735cf97d63317918b40084184517
Anjali Ahuja <anjaliannahuja@gmail.com>
2015-07-09 18:52:03 -0500

    new git

---
+++ 7-7/script.js
@@ -0,0 +1,6 @@
+$(function(){
+  var avengers = ["Iron Man", "Thor", "Hulk", "Ant Man", "Wasp"];
+
+  // Code goes here!
+
+});
---
+++ Functional Methods Project/script.js
@@ -0,0 +1,87 @@
+var oldGuardians = [
+    {
+      name: "Star Lord",
+      notes: "Team leader"
+    },
+    {
+      name: "Drax the Destroyer"
+    },
+    {
+      name: "Adam Warlock"
+    },
+    {
+      name: "Quasar",
+      notes: "Changed name to Martyr in Guardians of the Galaxy vol. 2 #12 (May 2009)."
+    },
+    {
+      name: "Rocket",
+      notes: "Served as temporary leader during Star-Lord's absence"
+    },
+    {
+      name: "Gamora"
+    }
+  ];
+  
+  
+
+  var newGuardians = [
+    {
+      name: "Mantis",
+      notes: "Served as an advisor beginning in Guardians of the Galaxy vol. 2 #1 (July 2008) before becoming an active member."
+    },
+    {
+      name: "Groot",
+      notes: "Appeared as a sapling beginning in Guardians of the Galaxy vol. 2 #1 (July 2008); joined active team after fully regrowing."
+    },
+    {
+      name: "Jack Flag"
+    },
+    {
+      name: "Quasar",
+      notes: "Changed name to Martyr in Guardians of the Galaxy vol. 2 #12 (May 2009)."
+    },
+    {
+      name: "Cosmo the Spacedog",
+      notes: "Assisted the team beginning in Guardians of the Galaxy vol. 2 #1 (July 2008) before officially joining team."
+    },
+    {
+      name: "Major Victory",
+      notes: "Also member of the alternate reality Guardians of the Galaxy team."
+    },
+    {
+      name: "Bug"
+    },
+    {
+      name: "Moondragon",
+      notes: "Resurrected by Drax and Phyla-Vell."
+    }
+  ];
+
+  var Guardians = $.merge(oldGuardians, newGuardians)
+
+  // Guardians = $.map(Guardians, function(name){
+  //   return name
+  // })
+
+  // Guardians = Guardians.filter(function(name){
+  //   return name != "Groot"
+  // })
+
+  console.log(Guardians)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
